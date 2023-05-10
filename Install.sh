a2127909746c1d7fa607de0dab784ffc47bcc69b
Inok <75870644+I-n-o-k@users.noreply.github.com>
2023-05-10 21:09:45 +0700

    Update Install script

--- Install.sh
+++ Install.sh
@@ -7,15 +7,16 @@ mkdir /data/local/ubuntu
 cd /data/local/ubuntu
 
 # Download rootfs 
-wget https://github.com/I-n-o-k/Ubuntu-20.04-chroot-Android/raw/master/ubuntu-focal-arm64-rootfs.tar.gz
+wget https://github.com/I-n-o-k/Ubuntu-chroot-Android/releases/download/22.04/ubuntu-22.04-arm64.tar.gz
 
 # Extract rootfs
-tar xpvf ubuntu-focal-arm64-rootfs.tar.gz
+tar xpvf *.tar.gz
 
 # Create a directory for Mountpoint internal storage
 mkdir sdcard
 
 # Download startup script 
+cd /data/local
 wget https://raw.githubusercontent.com/I-n-o-k/Ubuntu-20.04-chroot-Android/master/start-ubuntu.sh
 chmod +x start-ubuntu.sh
 
